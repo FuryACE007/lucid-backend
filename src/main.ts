e29537b423f652c3bfd17c992cebda63a6bc0494
FuryACE007 <sudhanshuvshekhar@gmail.com>
2023-12-22 21:35:33 +0530

    added cores access header

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-  })
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
